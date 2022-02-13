60ae567b82e5084a5ab8121427b38db1b625e028
Suniti <suniti0804>
2022-02-14 00:34:27 +0530

    Edited layout of Main Activity

--- app/src/main/java/com/project4/MainActivity.kt
+++ app/src/main/java/com/project4/MainActivity.kt
@@ -1,22 +1,21 @@
 package com.project4
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.project4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
-
 
     fun createBirthdayCard(view: View) {
 
